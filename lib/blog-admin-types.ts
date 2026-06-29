bcea7325bb32dd5dbab742d4012e529f4e34e9b3
Waqas Hanif Khan <waqasbangash7@gmail.com>
2026-06-29 14:49:54 +0500

    Add editor types

---
+++ lib/blog-admin-types.ts
@@ -0,0 +1,12 @@
+export interface BlogPostInput {
+  title: string;
+  slug: string;
+  excerpt: string;
+  publishedAt: string;
+  author: string;
+  category: string;
+  tags: string[];
+  seoTitle: string;
+  seoDescription: string;
+  content: string;
+}
