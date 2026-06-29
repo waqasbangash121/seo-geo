8a07e4d51ca8e6b6e24d2c0eb1f4ae34523fc003
Waqas Hanif Khan <waqasbangash7@gmail.com>
2026-06-29 19:21:41 +0500

    feat(resources): add resource creation page

---
+++ app/admin/(workspace)/resources/new/page.tsx
@@ -0,0 +1,5 @@
+import { ManagedContentEditorForm } from "@/components/managed-content-editor-form";
+
+export default function NewResourcePage() {
+  return <ManagedContentEditorForm type="resource" />;
+}
