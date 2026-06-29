6954d5d34e5413241be4b475d4e19496883f8440
Waqas Hanif Khan <waqasbangash7@gmail.com>
2026-06-29 11:29:01 +0500

    feat(cms): add CMS response middleware

---
+++ cms/config/middlewares.js
@@ -0,0 +1,13 @@
+module.exports = [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "global::noindex",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
