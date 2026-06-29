8dcf36896d1561614b0071cff98d0f7449e304ad
Waqas Hanif Khan <waqasbangash7@gmail.com>
2026-06-29 12:23:51 +0500

    fix(cms): trust reverse proxy in production

--- cms/config/server.js
+++ cms/config/server.js
@@ -2,7 +2,9 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("PUBLIC_URL", "http://localhost:1337"),
-  proxy: true,
+  proxy: {
+    koa: env.bool("IS_PROXIED", false),
+  },
   app: {
     keys: env.array("APP_KEYS"),
   },
